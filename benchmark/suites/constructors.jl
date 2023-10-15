4178c18898a49dc100306168b1099fe4c8073845
pedromxavier <pedromxavier@poli.ufrj.br>
2023-10-15 16:58:31 -0400

    Fix name

--- benchmark/suites/constructors.jl
+++ benchmark/suites/constructors.jl
@@ -1,4 +1,4 @@
-function benchmark_constructors!(suite, ::Type{F}) where {V,T,F<:PBO.AbstractPBF{V,T}}
+function benchmark_constructors!(suite, ::Type{F}) where {V,T,F<:PBO.AbstractFunction{V,T}}
     suite["constructors"] = BenchmarkGroup()
 
     return nothing
