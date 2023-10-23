85d35db5990db29557db89f189bfd3c81fc39176
pedromxavier <pedromxavier@poli.ufrj.br>
2023-10-23 14:10:51 -0400

    Fix deployment url

--- docs/make.jl
+++ docs/make.jl
@@ -40,7 +40,7 @@ if "--skip-deploy" ∈ ARGS
     @warn "Skipping deployment"
 else
     deploydocs(
-        repo = raw"github.com/pedromxavier/PseudoBooleanOptimization.jl.git",
+        repo = raw"github.com/psrenergy/PseudoBooleanOptimization.jl.git",
         push_preview = true,
     )
 end
