a003e20b718f0aef848005ed1f89e3f755287a31
David E. Bernal Neira <dbernaln@purdue.edu>
2026-06-23 19:13:46 -0400

    fix: complete quadratized XORSAT planted solutions (#28)

--- test/unit/synthesis.jl
+++ test/unit/synthesis.jl
@@ -3,6 +3,7 @@ using Random
 function test_synthesis()
     @testset "Synthesis" verbose = true begin
         test_regular_xorsat_planted_solutions()
+        test_quadratized_regular_xorsat_planted_solutions()
     end
 
     return nothing
@@ -21,6 +22,31 @@ function test_regular_xorsat_planted_solutions()
             x = only(solutions)
 
             @test Set(keys(x)) == Set(PBO.varmap(V, i) for i = 1:n)
+            @test all(v -> v in (0, 1), values(x))
+
+            y = f(x)
+
+            @test PBO.isconstant(y)
+            @test y[nothing] ≈ -n
+        end
+    end
+
+    return nothing
+end
+
+function test_quadratized_regular_xorsat_planted_solutions()
+    @testset "Quadratized regular XORSAT planted solutions" begin
+        for (V, seed) in ((Int, 33), (Symbol, 34))
+            n = 6
+            k = 3
+            F = PBO.TermDictPBF{V,Float64}
+            rng = Random.MersenneTwister(seed)
+            quad = PBO.Quadratization(PBO.DEFAULT(); stable = true)
+
+            f, solutions = PBO.k_regular_k_xorsat(rng, F, n, k; quad)
+            x = only(solutions)
+
+            @test Set(keys(x)) == Set(PBO.variables(f))
             @test all(v -> v in (0, 1), values(x))
 
             y = f(x)
